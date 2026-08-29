a3e6e0e715826ec7d935b439a168e08c5b30c357
sdsmnc221 <antr.2201@gmail.com>
2026-08-29 15:11:11 +0200

    chore(app): nwjs-jp-shim move shim.js.txt to shim.js welp

--- nwjs-joiplay-patch-shim/src/adapters/mz.js
+++ nwjs-joiplay-patch-shim/src/adapters/mz.js
@@ -169,9 +169,7 @@ export async function packPatchRpgMakerMz(
   const gameCfgContent = `title=${gameTitle}\nid=${gameId}\nexecFile=index.html\ntype=${gameType}`;
 
   // this is the shim.js
-  const shimJsContentTemplate = await readTemplate(
-    "./adapters/utils/shim.js.txt",
-  );
+  const shimJsContentTemplate = await readTemplate("./adapters/utils/shim.js");
   const shimJsContent = fill(shimJsContentTemplate, {
     ENGINE: detectionResult.engine,
   });
